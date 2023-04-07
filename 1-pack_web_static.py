27a94efdd35edc42aac540f75537c130ff7b55b0
Samuelibudi <ibudisamuel@gmail.com>
2023-04-07 12:53:55 +0000

    Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,13 +8,12 @@ import os
 
 
 def do_pack():
-    try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-            t = datetime.now()
-            f = "%Y%m%d%H%M%S"
-            archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-            local('tar -cvzf {} web_static'.format(archive_path))
-            return archive_path
-    except:
-        return None
+    if not os.path.exists("versions"):
+        local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        result = local('tar -cvzf {} web_static'.format(archive_path))
+        if result.failed:
+            return None
+        return result
